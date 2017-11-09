773df37157bffc9f0a79d4c99644d844c4bdf477
Connor Brinton <connor.brinton@gmail.com>
2017-11-08 21:33:47 -0800

    Reorganize folder structure and add sbatch file.

---
+++ default/train.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+python ../tensorflow-wavenet/train.py \
+  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/ \
+  --store_metadata True \
+  --histograms True \
+  --silence_threshold 0.0 \
+  --logdir /farmshare/user_data/connorb3/deeperpop/model/default/
---
+++ initialize-virtualenv.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Initialize the virtual environment
+virtualenv --system-site-packages .
+
+# Enter the virtual environment
+source ./bin/activate
+
+# Ensure pip is installed (and up-to-date)
+easy_install -U pip
+
+# Make sure we have the good protobuf
+pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.1.0-cp27-none-linux_x86_64.whl
+
+# Install tensorflow
+pip install --upgrade tensorflow-gpu
+
