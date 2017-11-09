bd576ef0a9aaad95ad9d0690aa05c2c4c5340efc
Connor Brinton <connor.brinton@gmail.com>
2017-11-08 22:21:20 -0800

    Remove histogram functionality for now.

--- default/train.sh
+++ default/train.sh
@@ -2,6 +2,5 @@
 python ../tensorflow-wavenet/train.py \
   --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/ \
   --store_metadata True \
-  --histograms True \
   --silence_threshold 0.0 \
   --logdir /farmshare/user_data/connorb3/deeperpop/model/default/
