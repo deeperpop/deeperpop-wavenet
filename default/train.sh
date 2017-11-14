2e8c602974c0dcd3d5d1a512862e20b5d29dbbae
Connor Brinton <connor.brinton@gmail.com>
2017-11-13 21:25:44 -0800

    Split data into train and test.

--- default/train.sh
+++ default/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 python ../tensorflow-wavenet/train.py \
-  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/ \
+  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/train/ \
   --store_metadata True \
   --silence_threshold 0.0 \
   --logdir /farmshare/user_data/connorb3/deeperpop/model/default/
