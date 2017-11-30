215f885b2ef65606d761486f81021a0943d62edc
Connor Brinton <connor.brinton@gmail.com>
2017-11-30 13:01:58 -0800

    Add stretched configurations.

---
+++ stretched-beat/train.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+python ../tensorflow-wavenet/train.py \
+  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/stretched/ \
+  --store_metadata True \
+  --silence_threshold 0.0 \
+  --logdir /farmshare/user_data/connorb3/deeperpop/model/stretched-beat/
