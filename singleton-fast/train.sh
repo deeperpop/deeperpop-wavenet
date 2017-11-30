da0b9dd298bb77d7f7021e07ea44d9dd3bc6b681
Connor Brinton <connor.brinton@gmail.com>
2017-11-30 01:06:12 -0800

    Fix incorrect data directory for singleton-fast.

--- singleton-fast/train.sh
+++ singleton-fast/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 python ../tensorflow-wavenet/train.py \
-  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/singleton-fast/ \
+  --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/singleton/ \
   --learning_rate 0.01 \
   --silence_threshold 0.0 \
   --logdir /farmshare/user_data/connorb3/deeperpop/model/singleton-fast/
