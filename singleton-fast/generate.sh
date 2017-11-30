e85a5b65095e7beb66f79989cd377a6e578fa796
Connor Brinton <connor.brinton@gmail.com>
2017-11-30 00:58:52 -0800

    Add fast and slow singleton configurations.

---
+++ singleton-fast/generate.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Declare variables (parameters)
+TENSORFLOW_WAVENET_PATH="/farmshare/user_data/connorb3/deeperpop/deeperpop-wavenet/tensorflow-wavenet/"
+MODEL_PATH="/farmshare/user_data/connorb3/deeperpop/model/singleton-fast/"
+SEED_PATH="/farmshare/user_data/connorb3/deeperpop/data/wav/seeds/singleton1.wav"
+OUTPUT_PATH="/farmshare/user_data/connorb3/deeperpop/results/singleton-fast/"
+
+# Select sample number
+SAMPLE_NUMBER="1"
+SAMPLE_PATH="$OUTPUT_PATH/$SAMPLE_NUMBER"
+
+# Create sample output path
+mkdir "$SAMPLE_PATH"
+
+# Perform generation
+python "${TENSORFLOW_WAVENET_PATH}generate.py" \
+    --samples 160000 \
+    --logdir "$SAMPLE_PATH" \
+    --wav_seed "$SEED_PATH" \
+    --wav_out_path "$SAMPLE_PATH/sample.wav" \
+    "${MODEL_PATH}model.ckpt-99999"
