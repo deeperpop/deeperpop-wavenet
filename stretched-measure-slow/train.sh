1d6e957e42ddf28b1c3a0347080f47156a1ac124
Connor Brinton <connor.brinton@gmail.com>
2017-11-30 22:59:40 -0800

    Fix model output directory for stretched/slow conditions.

--- stretched-measure-slow/train.sh
+++ stretched-measure-slow/train.sh
@@ -3,4 +3,4 @@ python ../tensorflow-wavenet/train.py \
   --data_dir /farmshare/user_data/connorb3/deeperpop/data/wav/stretched/ \
   --silence_threshold 0.0 \
   --learning_rate 0.0001 \
-  --logdir /farmshare/user_data/connorb3/deeperpop/model/stretched-measure/
+  --logdir /farmshare/user_data/connorb3/deeperpop/model/stretched-measure-slow/
